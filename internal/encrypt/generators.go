eb3cbea0020b4883ffc27671414e51af125b4de7
Roma Fominih <roma811@icloud.com>
2024-12-15 08:53:13 +0200

    Add RSA key pair generation to encryption module
    - Introduced GenerateRSAKeyPair function in internal/encrypt/generators.go to create RSA key pairs.
    - Implemented error handling for key pair generation and public key encoding failures.
    - Added RSAKeyPair struct to encapsulate the private and public keys in PEM format.

--- internal/encrypt/generators.go
+++ internal/encrypt/generators.go
@@ -2,8 +2,16 @@ package encrypt
 
 import (
 	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
 	"encoding/hex"
+	"encoding/pem"
 )
+
+type RSAKeyPair struct {
+	PrivateKey string
+	PublicKey  string
+}
 
 func GenerateSecretKey(length int) (string, error) {
 	if length != 16 && length != 32 {
@@ -24,4 +32,38 @@ func GenerateSecretKey(length int) (string, error) {
 	}
 
 	return hex.EncodeToString(key), nil
+}
+
+func GenerateRSAKeyPair() (*RSAKeyPair, error) {
+	privateKey, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		return nil, &EncryptError{
+			Message: err.Error(),
+			Reason:  "Key pair generation failed",
+			Code:    "KEYPAIR_ERROR",
+		}
+	}
+	privateKeyBytes := x509.MarshalPKCS1PrivateKey(privateKey)
+	privateKeyPEM := pem.EncodeToMemory(&pem.Block{
+		Type:  "RSA PRIVATE KEY",
+		Bytes: privateKeyBytes,
+	})
+
+	publicKeyBytes, err := x509.MarshalPKIXPublicKey(&privateKey.PublicKey)
+	if err != nil {
+		return nil, &EncryptError{
+			Message: err.Error(),
+			Reason:  "Public key encoding failed",
+			Code:    "KEYPAIR_ERROR",
+		}
+	}
+	publicKeyPEM := pem.EncodeToMemory(&pem.Block{
+		Type:  "PUBLIC KEY",
+		Bytes: publicKeyBytes,
+	})
+
+	return &RSAKeyPair{
+		PrivateKey: string(privateKeyPEM),
+		PublicKey:  string(publicKeyPEM),
+	}, nil
 }
